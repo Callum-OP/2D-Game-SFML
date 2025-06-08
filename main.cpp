371770d0ab5a4d2d77a084bb72e4f28450903b38
Callum-OP <callumoprey@gmail.com>
2025-06-09 00:23:22 +0100

    Draw rectangle in middle of window

--- main.cpp
+++ main.cpp
@@ -10,19 +10,23 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode({640, 480}), "2D Game", sf::Style::Titlebar | sf::Style::Close);
 
-    // Create a rectangle
-    sf::RectangleShape rectangle(sf::Vector2f(100.f, 50.f)); // Width: 100, Height: 50
-    rectangle.setFillColor(sf::Color::Green); // Set its color
+    // Create a rectangle in centre of window
+    sf::RectangleShape rectangle(sf::Vector2f(100.f, 50.f)); // Set width and height
+    rectangle.setFillColor(sf::Color::Green); // Set colour
+    sf::Vector2<float> position(275.f, 200.f); // Set coordinates
+    rectangle.setPosition(position); // Place rectangle at coordinates
 
-    while (window.isOpen())
-    {
-        while (auto event = window.pollEvent()) 
-        {
-            if (auto closeEvent = event->getIf<sf::Event::Closed>())
+    while (window.isOpen()) {
+        while (auto event = window.pollEvent()) {
+            // If window is closed
+            if (auto closeEvent = event->getIf<sf::Event::Closed>()) {
                 window.close();
+            }
         }
 
+        // Create new window with rectangle drawn in
         window.clear();
+        window.draw(rectangle);
         window.display();
     }
     return 0;
