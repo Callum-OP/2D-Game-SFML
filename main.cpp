c7a931ed576a4a2745b07a3deeb487d20ddf9c9a
Callum-OP <callumoprey@gmail.com>
2025-06-10 15:56:09 +0100

    Can now move rectangle using arrow keys

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({640, 480}), "2D Game", sf::Style::Titlebar | sf::Style::Close);
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "2D Game", sf::Style::Titlebar | sf::Style::Close);
+    window.setFramerateLimit(60);
 
     // Create a rectangle in centre of window
     sf::RectangleShape rectangle(sf::Vector2f(100.f, 50.f)); // Set width and height
@@ -16,18 +17,44 @@ int main()
     sf::Vector2<float> position(275.f, 200.f); // Set coordinates
     rectangle.setPosition(position); // Place rectangle at coordinates
 
+    // Game loop
+    const auto onClose = [&window](const sf::Event::Closed&) {
+        window.close();
+    };
+
+    const auto onKeyPressed = [&window](const sf::Event::KeyPressed& keyPressed) {
+        if (keyPressed.scancode == sf::Keyboard::Scancode::Escape)
+            window.close();
+    };
+
     while (window.isOpen()) {
-        while (auto event = window.pollEvent()) {
-            // If window is closed
-            if (auto closeEvent = event->getIf<sf::Event::Closed>()) {
-                window.close();
-            }
-        }
+        window.handleEvents(onClose, onKeyPressed);
+
+        // Remainder of main loop
 
         // Create new window with rectangle drawn in
         window.clear();
         window.draw(rectangle);
         window.display();
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
+            // If left key pressed then move character left
+            rectangle.move({-1.f, 0.f});
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Right))
+        {
+            // If right key pressed then move character right
+            rectangle.move({1.f, 0.f});
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Down))
+        {
+            // If down key pressed then move character down
+            rectangle.move({0.f, 1.f});
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Up))
+        {
+            // If up key pressed then move character up
+            rectangle.move({0.f, -1.f});
+        }
     }
-    return 0;
 }
