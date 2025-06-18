1ed19aa54d53c490fdbf5bc3f0f3c230a31ffb13
Callum-OP <callumoprey@gmail.com>
2025-06-18 23:28:46 +0100

    Updated sprint calling code to make it more consistent and accurate

--- main.cpp
+++ main.cpp
@@ -18,21 +18,23 @@ int main()
     sf::RenderWindow window(sf::VideoMode({800, 600}), "2D Game", sf::Style::Titlebar | sf::Style::Close);
     window.setFramerateLimit(60);
 
+    // Timers and keys used for detecting double taps
     sf::Clock clock;
-    sf::Time lastClickTime;
-    const sf::Time doubleClickTime = sf::milliseconds(500);
-    sf::Keyboard::Scancode lastDirection = sf::Keyboard::Scancode::Unknown;
+    sf::Time lastClickTime; // The last click of each key
+    const sf::Time doubleClickTime = sf::milliseconds(500); // Expected time limit for second click to happen
+    sf::Keyboard::Scancode lastDirection = sf::Keyboard::Scancode::Unknown; // The direction of last key press
 
     // Ensures window closes properly when closed
     const auto onClose = [&window](const sf::Event::Closed&) {
         window.close();
     };
     // Check when key is pressed
-    const auto onKeyPressed = [&window, &player, &clock, &lastClickTime, &doubleClickTime](const sf::Event::KeyPressed& keyPressed) {
+    const auto onKeyPressed = [&window, &lastDirection](const sf::Event::KeyPressed& keyPressed) {
         // Ensure window is closed when Escape key is pressed
         if (keyPressed.scancode == sf::Keyboard::Scancode::Escape) {
             window.close();
         }
+        lastDirection = keyPressed.scancode; // Key that was last clicked
     };
     // Check when key is released
     const auto onKeyReleased = [&window, &player, &clock, &lastClickTime, &doubleClickTime, &lastDirection](const sf::Event::KeyReleased& keyPressed) {
@@ -45,15 +47,16 @@ int main()
             keyPressed.scancode == sf::Keyboard::Scan::W || 
             keyPressed.scancode == sf::Keyboard::Scan::Down || 
             keyPressed.scancode == sf::Keyboard::Scan::S) {
-                
-            sf::Time now = clock.getElapsedTime();
+            
+            sf::Time now = clock.getElapsedTime(); // Current time
+            // Sprint only if within expected click time and facing same direction as last click
             if (now - lastClickTime < doubleClickTime && keyPressed.scancode == lastDirection) {
                 player.sprint(true);
             } else {
                 player.sprint(false);
             }
-            lastClickTime = now;
-            lastDirection = keyPressed.scancode;
+            lastDirection = keyPressed.scancode; // Key that was last clicked
+            lastClickTime = now; // Time user last clicked
         }
     };
 
